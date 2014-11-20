9b130d455ea2a61a6dbbad8c194346cf77898cf1
srprice3 <srprice3@users.noreply.github.com>
2014-11-20 08:28:39 -0500

    Create simhttp.h

---
+++ simhttp.h
@@ -0,0 +1,35 @@
+#ifndef SIMHTTP_H
+#define SIMHTTP_H
+
+/*********************************************************
+*
+* Stephen Price   CpSc3600  HW3
+* Dr. Remy Sokou, Fall 2014
+* File Name:    simhttp.h	
+*
+* Summary:
+*  This file contains common stuff for the server
+*
+* Revisions:
+*
+*********************************************************/
+#include <stdio.h>
+#include <signal.h>
+#include <errno.h>
+#include <time.h>
+//#include <getopt.h>
+#include <ctype.h>
+#include <sys/time.h>
+#include <math.h>
+#include <sys/types.h>
+#include <string.h>     /* for memset() */
+#include <netinet/in.h> /* for in_addr */
+#include <sys/socket.h> /* for socket(), connect(), sendto(), and recvfrom() */
+#include <arpa/inet.h>  /* for sockaddr_in and inet_addr() */
+#include <stdlib.h>     /* for atoi() and exit() */
+#include <unistd.h>     /* for close(), getopt(), chdir() */
+
+
+int readData(void);
+
+#endif
